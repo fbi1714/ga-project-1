c9c23ad9a82fe9f45fd191e9e4b0f35694a8ef11
fbi1714 <fcamatti@ucs.br>
2016-06-02 11:33:45 +1000

    Try 1 to fix Heroku issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,5 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'bower_components')  
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'bower_components')
+Rails.application.config.assets.precompile << /materialize\/font\/.+\.(?:svg|eot|woff|woff2|ttf)\z/  
